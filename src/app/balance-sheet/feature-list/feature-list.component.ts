8b5951b8f2916b486e1730534f33067924d53655
bsarnii <scharnoui@gmail.com>
2024-09-10 16:28:31 +0200

    chg: implemented pdf download

--- src/app/balance-sheet/feature-list/feature-list.component.ts
+++ src/app/balance-sheet/feature-list/feature-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, ElementRef, inject, ViewChild } from '@angular/core';
 import { BalanceSheetFacade } from '../data-access/balance-sheet.facade';
 import { ItemCategoryEnum, BalanceSheetItem } from '../models/balance-sheet-item.model';
 import { BalanceSheetSectionComponent } from '../ui/balance-sheet-section/balance-sheet-section.component';
+import * as jspdf from 'jspdf';
 
 @Component({
   selector: 'app-feature-list',
@@ -12,6 +13,8 @@ import { BalanceSheetSectionComponent } from '../ui/balance-sheet-section/balanc
 })
 export class FeatureListComponent {
   facade = inject(BalanceSheetFacade);
+
+  @ViewChild('balanceSheetWrapper') balanceSheet!:ElementRef<HTMLDivElement>
   
   ngOnInit(){
     this.facade.loadData();
@@ -30,6 +33,18 @@ export class FeatureListComponent {
       {} as Record<string,BalanceSheetItem[]>
   );
   })
+
+  generatePDF(){
+    const el = this.balanceSheet.nativeElement;
+    const pdf = new jspdf.jsPDF('p', 'pt', 'a4');
+    pdf.html(el, {
+      margin: [10, 10, 10, 10], // Adjust margins to ensure content fits
+      callback:function (el) {
+        window.open(pdf.output('bloburl'))
+        el.save()
+      }
+    })
+  }
 
 }
 
